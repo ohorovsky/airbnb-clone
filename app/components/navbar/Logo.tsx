94c30f32930a081a7417764afc2e987776d27590
Ondrej Horovsky <ondrej.h@day8.com>
2023-04-16 17:10:36 +0100

    started project, navbar components

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image alt="logo" className='hidden md:block cursor-pointer' src="/images/logo.png" width={100} height={100} />
+  )
+}
+
+export default Logo;
