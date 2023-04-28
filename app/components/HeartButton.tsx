da87d8caf964e54562650b8bf0c5175b938ac3c8
ohorovsky <o.horovsky@gmail.com>
2023-04-28 14:46:54 +0200

    added listings

---
+++ app/components/HeartButton.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { SafeUser } from "../types";
+
+interface HeartButtonProps {
+  listingId: string
+  currentUser?: SafeUser | null
+}
+
+const HeartButton: React.FC<HeartButtonProps> = ({ listingId, currentUser }) => {
+  const hasFavourited = false;
+  const toggleFavourite = () => { };
+  return (
+    <div className="relative hover:opacity-80 transition cursor-pointer" onClick={toggleFavourite}>
+      <AiOutlineHeart className="fill-white absolute -top-[2px] -right-[2px]" size={28} />
+      <AiFillHeart className={`${hasFavourited ? "fill-rose-500" : "fill-neutral-500/70"}`} size={24} />
+    </div>
+  )
+}
+
+export default HeartButton;
