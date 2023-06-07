a370a2587f4d250ceceb8af4e30279d135578d24
Ondrej Horovsky <ondrej@DAY8-42.local>
2023-06-07 19:17:10 +0100

    adding trips page

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -64,7 +64,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
         toast.success('Reservation created successfully');
         setDateRange(initialDateRange);
         //redirect to /trips
-        router.refresh();
+        router.push('/trips');
       })
       .catch(() => toast.error('Something went wrong'))
       .finally(() => setIsLoading(false));
---
+++ app/trips/TripsClient.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { useRouter } from "next/navigation";
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import { SafeReservation, SafeUser } from "../types";
+import { useCallback, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import ListingCard from "../components/listings/ListingCard";
+
+interface TripsClientProps {
+  reservations: SafeReservation[]
+  currentUser: SafeUser
+}
+
+const TripsClient: React.FC<TripsClientProps> = ({ reservations, currentUser }) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState<string>('');
+  const onCancel = useCallback((id: string) => {
+    setDeletingId(id)
+    axios.delete(`/api/reservations/${id}`)
+      .then(() => {
+        toast.success('Reservation cancelled');
+      })
+      .catch(e => {
+        toast.error('Something went wrong. Message: ' + e?.response?.data?.error)
+        router.refresh();
+      })
+      .finally(() => {
+        setDeletingId('')
+      });
+  }, [router])
+  return (
+    <Container>
+      <Heading title="Trips" subtitle=" Where you've been and where you're going" />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {reservations.map(reservation => (
+          <ListingCard key={reservation.id} data={reservation.listing} reservation={reservation} actionId={reservation.id} onAction={onCancel} actionLabel="Cancel reservation" disabled={deletingId === reservation.id} currentUser={currentUser} />
+        ))}
+      </div>
+    </Container>
+  )
+};
+export default TripsClient;
